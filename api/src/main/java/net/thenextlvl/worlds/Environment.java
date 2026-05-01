0debcf968ab009fd718b29b39a23f3689028b456
david <mail@nonswag.dev>
2026-05-01 15:58:20 +0200

    Add environments stream

--- api/src/main/java/net/thenextlvl/worlds/Environment.java
+++ api/src/main/java/net/thenextlvl/worlds/Environment.java
@@ -3,6 +3,8 @@ package net.thenextlvl.worlds;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.key.Keyed;
 
+import java.util.stream.Stream;
+
 sealed public interface Environment extends Keyed permits SimpleEnvironment {
     Environment OVERWORLD = of(Key.key("overworld"));
     Environment THE_END = of(Key.key("the_end"));
@@ -10,5 +12,9 @@ sealed public interface Environment extends Keyed permits SimpleEnvironment {
 
     static Environment of(final Key key) {
         return new SimpleEnvironment(key);
+    }
+
+    static Stream<Environment> environments() {
+        return Stream.of(OVERWORLD, THE_NETHER, THE_END);
     }
 }
